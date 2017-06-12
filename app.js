0c78c821c91ad4897e2b785bcc48414552afc22a
Manderson55 <mariela.z.anderson@gmail.com>
2017-06-12 14:10:25 -0600

    Added animation changes

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 $(document).ready(function(){
  
-    var topics = ["dresses", "skirts", "women's pants", "blouses", "baby onesies", "wedding gowns"];
+    var topics = ["rings", "flowers", "cars", "gardens", "castles", "wedding gowns"];
    
- //   var clothes = $(this).att("data-name");
- //       alert("you clicked " + topics );
- 
-      // Function for displaying the buttons
- 
-
+    // Function for displaying the buttons
     function renderButtons() {
   
-        $("#clothesView").empty();
+        $("#topicsView").empty();
 
         for (var i = 0; i < topics.length; i++) {
             var isIndexEven = true; 
@@ -22,42 +17,46 @@ $(document).ready(function(){
                   isIndexEven= false;
                }
               } 
-        isEven(i);
+        isEven(i); //calling isEven function to display different color buttons
+                   //if the index is odd or even 
 
         if (isIndexEven){
             var a = $("<button>");
             a.addClass("topics btn btn-primary");
             a.attr("data-name", topics[i]);
             a.text(topics[i]);
-            $("#clothesView").append(a);
+            $("#topicsView").append(a);
+            $("#topicsView").append(" ");
          } else {
               var a = $("<button>");
-              a.addClass("topics btn btn-warning");
+              a.addClass("topics btn btn-success");
               a.attr("data-name", topics[i]);
               a.text(topics[i]);
-              $("#clothesView").append(a);
+              $("#topicsView").append(a);
+              $("#topicsView").append(" ");
             }
         }
-    }
+    }// end render buttons
+
       // This function handles events where one button is clicked
-    $("#addClothes").on("click", function(event) {
+    $("#addTopic").on("click", function(event) {
         event.preventDefault();
-        var clothesAdd = $("#clothesInput").val().trim();
-        if (clothesAdd === "") {
+        var topicsAdd = $("#topicsInput").val().trim();
+        if (topicsAdd === "") {
           alert ("you need to input a new item!");
         } else {
-        topics.push(clothesAdd);
-        $("#clothesForm").get(0).reset();
-        renderButtons();
+          topics.push(topicsAdd);
+          $("#topicsForm").get(0).reset();
+          renderButtons();
         }
       });
 
-      // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+   // Calling the renderButtons function to display the intial buttons
+   renderButtons();
 
    $("button").on("click", function() {
-
-       $("gifs-appear-here").empty(); //clears the previous gifs      
+      event.preventDefault();
+          $("gifs-appear-here").empty(); //clears the previous gifs   
 
        var itemSelected = $(this).attr("data-name");
        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
@@ -69,21 +68,47 @@ $(document).ready(function(){
       })
      .done(function(response) {
         console.log(response);
+   
          var results = response.data; //store all the arrays in an array called results
+         var topicSelected = $("<h3>").text(itemSelected);
+         $("#gifs-appear-here").append(topicSelected); //printing button Selected
          for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div class='item'>");
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
-            var itemImage = $("<img>");
-            itemImage.attr("src", results[i].images.fixed_height_small_still.url);
+            var itemImage = $("<img>").attr({
+                "src": results[i].images.fixed_height_small_still.url,
+                "class":"showImage",
+                "data-state":"still",
+                "data-still": results[i].images.fixed_height_small_still.url,
+                "data-animate": results[i].images.fixed_height_small.url
+                 });
             gifDiv.prepend(p);
             gifDiv.prepend(itemImage);
             $("#gifs-appear-here").append(gifDiv);
+            $("#gifs-appear-here").append(" ");
          }
-      });
+
+         // this is were we check the image state when the user clicks on the image
+          $(".showImage").on("click", function() {
+              console.log ("inside image click function");
+              var state = $(this).attr("data-state");
+              console.log(this, "clicked image is " + state);
+              if (state === "still") {
+                  console.log ("image is still, change to animate");
+                  $(this).attr("src", $(this).attr("data-animate"));
+                  $(this).attr("data-state", "animate");
+           
+              } else {
+                  console.log ("image is animated, change to still");             
+                  $(this).attr("src", $(this).attr("data-still"));
+                  $(this).attr("data-state", "still");
+              }
+         
+            }); //end gif onclick function to animate
+
+      }); // end of done function response
 
     }); // end button on click function
-
-
 
 }); //end document ready
